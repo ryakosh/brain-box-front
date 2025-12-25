f2ac576a1872e940062a429da6e0aae650d1c78a
ryakosh <aryashokri@duck.com>
2025-12-25 15:32:29 +0330

    fix: redirecting to login when offline

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,11 +1,12 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { onlineManager } from "@tanstack/react-query";
 
 import { useAuth } from "@/context/AuthContext";
 
 export const ProtectedRoute = () => {
   const { isAuthenticated, isLoading } = useAuth();
 
-  if (!isLoading && !isAuthenticated) {
+  if (!isLoading && !isAuthenticated && onlineManager.isOnline()) {
     return <Navigate to="/auth/login" replace />;
   }
 
